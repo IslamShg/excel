6ec909749cde00b88da0dafda442e7adbf73381b
IslamShg <islam-shagae@mai.ru>
2021-03-07 13:46:24 +0300

    comment debug

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,7 +12,10 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      window['c' + component] = component 
+      // DEBUG
+      // if (component.name) {
+      //   window['c' + component.name] = component
+      // }
       $el.html(component.toHTML())
       $root.append($el)
       return component
