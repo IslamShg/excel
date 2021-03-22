3234806d6e307eecbb3d365194ae2d2793e94bf5
IslamShg <islam-shagae@mai.ru>
2021-03-22 21:18:03 +0300

    imlement DIP

--- src/core/page/Page.js
+++ src/core/page/Page.js
@@ -1,6 +1,6 @@
 export class Page {
   constructor(params) {
-    this.params = params
+    this.params = params || Date.now().toString()
   }
 
   getRoot() {
--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,33 +1,33 @@
-import {Page} from '@core/Page'
+import {Page} from '@core/page/Page'
 import {Excel} from '@/components/excel/Excel'
 import {Header} from '@/components/header/Header'
 import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
 
+import {StateProcessor} from '../core/page/StateProcessor'
+import {LocalStorageClient} from './../shared/LocalStorageClient'
+
 import {createStore} from '@/core/store/createStore'
 import {rootReducer} from '@/redux/rootReducer'
-import {storage, debounce} from '@core/utils'
 import {normalizeInitialState} from '@/redux/initialState'
 
-
-function storageName(param) {
-  return 'excel:' + param
-}
-
 export class ExcelPage extends Page {
-  getRoot() {
-    const params = this.params
-    
-    const state = storage(storageName(params))
-    const initialState = normalizeInitialState(state)
+  constructor(param) {
+    super(param)
+ 
+    this.storeSub = null
+    this.processor = new StateProcessor(
+      new LocalStorageClient(this.params)
+    )
+  }
 
+  async getRoot() {
+    const state = await this.processor.get()
+    const initialState = normalizeInitialState(state)
     const store = createStore(rootReducer, initialState)
 
-    const stateListener = debounce(state => {
-      storage(storageName(params), state)
-    }, 300)
-    store.subscribe(stateListener)
+    this.storeSub = store.subscribe(this.processor.listen)
 
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
@@ -43,5 +43,6 @@ export class ExcelPage extends Page {
 
   destroy() {
     this.excel.destroy()
+    this.storeSub.unsubscribe()
   }
 }
